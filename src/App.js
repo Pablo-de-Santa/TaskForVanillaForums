662d28ee445cdf4d6a4052e4eb771f94697fd940
Paul Nohtiev <nogtevpasha97@gmail.com>
2021-01-13 19:35:59 -0500

    Add search functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Typography, Box, TextField, Button } from '@material-ui/core';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [repositories, setRepositories] = useState([]);
+
+  function search() {
+    if (!username) {
+      setError('Enter username');
+      return;
+    }
+
+    setError(null);
+    setIsLoading(true);
+
+    axios.get(`https://api.github.com/users/${username}/repos`)
+      .then(response => {
+        setRepositories(response.data);
+        setIsLoading(false);
+        console.log(response.data);
+      })
+      .catch(() => {
+        setError('User doesn\'t exist');
+        setIsLoading(false);
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Box display='flex'>
+        <TextField
+          value={username}
+          disabled={isLoading}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Box ml={1}>
+          <Button
+            color='primary'
+            variant='contained'
+            onClick={search}
+            disabled={isLoading}
+          >
+            Search
+          </Button>
+        </Box>
+      </Box>
+      {error && (
+        <Typography color='error'>{error}</Typography>
+      )}
+    </Box>
   );
 }
 
